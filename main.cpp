3e01ada0066701f56bd5b0a0e42749bd0c95480b
4sayken-sz <shubhamyadav52000@gmail.com>
2026-01-15 00:51:31 +0530

    program-exec

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <sstream>
 #include <cstdlib>
 #include <filesystem>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+bool findExecFile(std::string, std::string);
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -14,62 +19,99 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
     
-    std::string userCommandInput = "";
-    std::string command = "";
-    std::string parameters = "";
-    
     std::cout << "$ ";
+    std::string userCommandInput = "";
     std::getline(std::cin, userCommandInput);
-    std::istringstream parseCommand(userCommandInput);
 
-    parseCommand >> command;
-    std::getline(parseCommand >> std::ws, parameters);
-    if(command == "echo") std::cout << parameters << std::endl;
-    else if(command == "type") {
-      std::string parameterCmd = "";
-      std::istringstream parseTemp(parameters);
-      parseTemp >> parameterCmd;
+    std::string cmd = "";
+    std::string paramStr = "";
+    std::istringstream parseUserCommand(userCommandInput);
+
+    parseUserCommand >> cmd;
+    if (cmd == "exit") return 0;
+
+    std::getline(parseUserCommand >> std::ws, paramStr);
+    if(cmd == "echo") std::cout << paramStr << std::endl;
+    else if(cmd == "type") {
       bool foundcmd = false;
+      std::string parameterCmd = "";
+      std::istringstream parseTemp(paramStr);
 
-      for(int i=0; i<commandstrings.size(); i++) {
-        if(commandstrings[i] == parameterCmd) {
-          std::cout << parameterCmd << " is a shell builtin" << std::endl;
-          foundcmd = true;
-          break;
+      if(parseTemp >> parameterCmd) {
+        for(int i=0; i<commandstrings.size(); i++) {
+          if(commandstrings[i] == parameterCmd) {
+            std::cout << parameterCmd << " is a shell builtin" << std::endl;
+            foundcmd = true;
+            break;
+          }
+        }
+
+        if(!foundcmd) {
+          const char* pathVal = std::getenv("PATH");
+          std::string Path = "";
+          std::istringstream pathParse(pathVal);
+
+          while(!foundcmd && std::getline(pathParse, Path, ':')) {
+            foundcmd = findExecFile(Path, parameterCmd);
+          }
+
+          if(foundcmd) std::cout << parameterCmd << " is " << Path << "/" << parameterCmd << std::endl;
+          else std::cout << parameterCmd << ": not found" << std::endl;
         }
       }
-
-      if(!foundcmd) {
+    } else {
+      std::istringstream parseProgram_Args(userCommandInput);
+      std::vector<std::string> argStrings;
+      std::string temp = "";
+      
+      bool foundcmd = false;
+      if(parseProgram_Args >> temp) {
         const char* pathVal = std::getenv("PATH");
+        std::string Path = "";
         std::istringstream pathParse(pathVal);
-        std::string currentPath = "";
-        if(pathVal != nullptr) {
-          while(std::getline(pathParse, currentPath, ':') && !foundcmd) {
-            std::filesystem::path currentFileSysPath = currentPath;
 
-            if(std::filesystem::exists(currentFileSysPath) && std::filesystem::is_directory(currentFileSysPath)) {
-              for(const auto &file : std::filesystem::directory_iterator(currentFileSysPath)) {
-                if(file.path().filename() == parameterCmd) {
-                  std::filesystem::file_status fileStatus = std::filesystem::status(file.path());
-                  std::filesystem::perms filePermissions = fileStatus.permissions();
-
-                  if((filePermissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none) {
-                    std::cout << parameterCmd << " is " << currentPath << "/" << parameterCmd << std::endl;
-                    foundcmd = true;
-                    break;
-                  }
-                }
-              }
-            } else std::cout << currentPath << " : directory doesn't exists" << std::endl;
+        while(!foundcmd && std::getline(pathParse, Path, ':')) {
+            foundcmd = findExecFile(Path, temp);
           }
-        } else std::cout << "not found path variables" << std::endl;
       }
 
-      if(!foundcmd) std::cout << parameterCmd << ": not found" << std::endl;
+      if(foundcmd) {
+        argStrings.push_back(temp);
+        while(parseProgram_Args >> temp) {
+          argStrings.push_back(temp);
+        }
+        
+        std::vector<char*> programArgs;
+        for(auto &strptr : argStrings) {
+          programArgs.push_back(const_cast<char*>(strptr.c_str()));
+        }
+        programArgs.push_back(nullptr);
+
+        pid_t pid = fork();
+        if(pid == 0) execvp(programArgs[0], programArgs.data());
+        else if(pid > 0) wait(NULL);
+      } else std::cout << temp << ": command not found" << std::endl;
     }
-    else if (command == "exit") return 0;
-    else std::cout << command << ": command not found" << std::endl;
   }
-  
   return 0;
+}
+
+bool findExecFile(std::string envPath, std::string cmdName) {
+  bool fileExists = false; 
+  std::filesystem::path currentFileSysPath = envPath;
+
+  if(std::filesystem::exists(currentFileSysPath) && std::filesystem::is_directory(currentFileSysPath)) {
+    for(const std::filesystem::directory_entry file : std::filesystem::directory_iterator(currentFileSysPath)) {
+      if(file.path().filename() == cmdName) {
+        std::filesystem::file_status fileStatus = std::filesystem::status(file.path());
+        std::filesystem::perms filePermissions = fileStatus.permissions();
+
+        if((filePermissions & std::filesystem::perms::owner_exec) != std::filesystem::perms::none) {
+          fileExists = true;
+        }
+      }
+    }
+  }
+
+  return fileExists;
 }
